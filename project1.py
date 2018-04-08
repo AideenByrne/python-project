fecd613d62be945324b34b2d5719e1d00a2c9310
AideenByrne <g00364780@gmit.ie>
2018-04-08 11:00:33 +0100

    More changes - fix merge problems

--- project1.py
+++ project1.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #Aideen Byrne 26th March 2018 
 #Code for investigating Iris Data Set for Programming & Scripting module project 
 #Select only the rows of the Virginica flowers and assign it to virginica 
@@ -11,16 +11,5 @@ species = df1[["Species"]] #to select column named Species
 
 # need to get the correct method for this function on the df1 object here
 virginica = isin(["Iris-virginica"]) #need to fix this - trying to select virginica flowers from species column
-=======
-#Aideen Byrne 26th March 2018 
-#Code for investigating Iris Data Set for Programming & Scripting module project 
-#Select only the rows of the Virginica flowers and assign it to virginica 
 
-import pandas as pd #import pandas library 
-df1 = pd.read_csv("data/iris.csv")  #label contents of iris.csv file as dataframe
-my_columns = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"] #borrowed from Jeff Tratner at https://stackoverflow.com/questions/17018638/assigning-column-names-from-a-list-to-a-table
-df1.columns = my_columns
-species = df1[["Species"]] #to select column named Species
-virginica = isin(["Iris-virginica"]) #need to fix this - trying to select virginica flowers from species column
->>>>>>> 6f3cab080b3ba50bca3d26fb641117c793e24783
 print (virginica)
