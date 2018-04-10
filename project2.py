4e11fdc3a83fe259ea310bbf88d224af0ec78012
AideenByrne <g00364780@gmit.ie>
2018-04-10 22:43:54 +0100

    new code

---
+++ project2.py
@@ -0,0 +1,6 @@
+#Aideen Byrne 10th April 2018
+#Using Numpy library to analyse Fisher's Iris data set
+
+import numpy as np #imports numpy library
+data = np.genfromtxt("data/iris.csv", delimiter = ',') #numpy opens iris.csv file
+
