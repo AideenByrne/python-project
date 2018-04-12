45b8ddbea956ef05ecc2e659fac83fd02da3f373
AideenByrne <g00364780@gmit.ie>
2018-04-12 21:30:01 +0100

    updated code

--- project2.py
+++ project2.py
@@ -3,4 +3,23 @@
 
 import numpy as np #imports numpy library
 data = np.genfromtxt("data/iris.csv", delimiter = ',') #numpy opens iris.csv file
-
+firstcol = data[:,0] #selects first column of file
+print (np.mean(firstcol))#prints mean of first column
+secondcol = data[:,1]
+print (np.mean(secondcol))
+thirdcol = data[:,2]
+print (np.mean(thirdcol))
+fourthcol = data[:,3]
+print (np.mean(fourthcol))
+print (max(firstcol))#prints max of first column
+print (min(firstcol))#prints mean of first column
+print (max(secondcol))
+print (min(secondcol))
+print (max(thirdcol))
+print (min(thirdcol))
+print (max(fourthcol))
+print (min(fourthcol))
+standarddev = np.array([firstcol,secondcol,thirdcol, fourthcol])#builds array of of all columns
+print (np.std(standarddev))#finds standard deviation of all columns
+import matplotlib.pyplot as plot #need to fix this!!
+print (plot.hist(standarddev))
