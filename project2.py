8f360825468a79f4bd399d57ae9b6e6cfad5883b
AideenByrne <g00364780@gmit.ie>
2018-04-14 14:36:46 +0100

    updated script with new histogram

--- project2.py
+++ project2.py
@@ -13,7 +13,7 @@ fourthcol = data[:,3]
 print ("The average Petal Width is:", np.mean(fourthcol))
 fifthcol = data[:,4]
 alllength = np.array([firstcol, thirdcol])#builds array of data related to length measurements
-allwidth = np.array([secondcol, fourthcol])
+allwidth = np.array([secondcol, fourthcol])#builds array of data related to width
 print ("The average of all length measurements is:", np.mean(alllength)) #prints mean of all length measurements
 print ("The average of all width measurements is:", np.mean(allwidth)) #prints mean of all width measurements
 print ("The maximum Sepal Length is:", max(firstcol))#prints max of first column
@@ -26,6 +26,8 @@ print ("The maximum Petal Width is:", max(fourthcol))
 print ("The minimum Petal Width is:", min(fourthcol))
 print ("The standard deviation of length measurement is:", np.std(alllength))#finds standard deviation of all length measurements
 print ("The standard deviation of all width measurements is:", np.std(allwidth))#finds standard deviation of all width measurements
+
+#to generate histogram representations of data 
 import matplotlib.pyplot as pl
 pl.hist(firstcol, color="red")#calls histogram of first column and displays in red
 pl.ylabel("Frequency")#labels y axis of histogram
@@ -42,4 +44,17 @@ pl.show()
 pl.hist(fourthcol, color="yellow")
 pl.ylabel("Frequency")
 pl.xlabel("Petal Width")
+pl.show()
+
+#To generate histogram representation of all measurements
+x = [firstcol]
+y = [secondcol]
+z = [thirdcol]
+w = [fourthcol]
+
+pl.hist(x, label="Sepal Length", color="red")
+pl.hist(y, label="Sepal Width", color="blue")
+pl.hist(z, label="Petal Length", color="green")
+pl.hist(w, label="Petal Width", color="yellow")
+pl.legend(loc='upper right')
 pl.show()
