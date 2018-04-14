bb129ccd3c7ff0ebcd0ced62993d3cc601c4b7ea
AideenByrne <g00364780@gmit.ie>
2018-04-14 12:20:45 +0100

    updated script with new code

--- project1.py
+++ project1.py
@@ -9,4 +9,22 @@ my_columns = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Spe
 df1.columns = my_columns
 species = df1[["Species"]] #to select column named Species
 virginica = df1.loc[(df1["Species"] == "Iris-virginica")] #select rows that contain virginica flowers in species column
-print (virginica)
+print (virginica)#prints only rows that contain virginica flowers in species column
+
+#Select only the Sepal Length of the Virginica flowers and assign it 
+vsepallength = virginica["Sepal Length"]
+print (vsepallength)
+
+#Calculate the mean, median, variance and standard deviation of the Virginica Sepal Length
+print ("The mean of Virginica Sepal Length is", vsepallength.mean())
+print ("The median of Virginica Sepal Length is", vsepallength.median())
+print ("The variance of Virginica Sepal Length is", vsepallength.var())
+print ("The standard deviation of Virginica Sepal Length is", vsepallength.std())
+
+#Select only the numerical columns
+selectnumcol = df1[["Sepal Length", "Sepal Width", "Petal Length", "Petal Width"]]
+print (selectnumcol)
+
+#Calculate the mean of all the numerical variables
+print ("The mean per column is", selectnumcol.mean())
+print ("The mean of all columns is", selectnumcol.mean().mean())
